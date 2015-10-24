267b338d0823d3d529609fefb4704d7cec992955
Administrator <admin@example.com>
2015-10-24 14:51:54 -0500

    adding junit testing

--- karma.conf.js
+++ karma.conf.js
@@ -46,12 +46,16 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec', 'progress', 'coverage'],
+    reporters: ['spec', 'progress', 'coverage', 'dots', 'junit'],
 
     coverageReporter: {
       type : 'html',
       // output coverage reports
       dir : 'coverage/'
+    },
+
+    junitReporter: {
+      outputFile: 'test-results.xml'
     },
 
 
@@ -79,6 +83,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   })
 }
